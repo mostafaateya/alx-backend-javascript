cc1a29abea57a91240919f19468e88985b4b09c8
mostafaateya <mostafaateya@gmail.com>
2024-03-06 22:53:05 +0000

    0. Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+function getResponseFromAPI() {
+  return new Promise((_resolve, _reject) => {
+    /*eslint-disable */
+    if (true) {
+      _resolve();
+    } else {
+      _reject();
+    }
+    /* eslint-enable */
+  });
+}
+
+export default getResponseFromAPI;
