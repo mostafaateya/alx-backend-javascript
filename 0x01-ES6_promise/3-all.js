e5d0991e449f0d877d81d0fc9e2491669cb9e076
mostafaateya <mostafaateya@gmail.com>
2024-03-06 23:06:02 +0000

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((_val) => {
-      const { _body } = _val[0];
-      const { _firstName } = _val[1];
-      const { _lastName } = _val[1];
-      console.log(`${_body} ${_firstName} ${_lastName}`);
+      const { body } = _val[0];
+      const { firstName } = _val[1];
+      const { lastName } = _val[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
